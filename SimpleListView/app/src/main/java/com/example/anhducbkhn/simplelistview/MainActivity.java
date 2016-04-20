019f6ebf2aae8a1c4be9001ad6ca645d74bc6370
root <anhduc.bkhn@gmail.com>
2016-04-20 15:32:49 +0700

    Add project listview

---
+++ SimpleListView/app/src/main/java/com/example/anhducbkhn/simplelistview/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.anhducbkhn.simplelistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final String arr[] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+                "Ten", "Ti", "Teo", "Ba", "Bo", "Test"};
+
+        lv = (ListView)findViewById(R.id.lv);
+
+        /**
+         * Build from existing layout simple_list_item_1
+         * Context is MainActivity.this or this
+         */
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
+
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+                Toast toast = Toast.makeText(MainActivity.this, "Position: " + i, Toast.LENGTH_LONG);
+                toast.show();
+
+            }
+        });
+    }
+}
